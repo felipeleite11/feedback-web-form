44858461809fd02922d89539b7501e3f9959daf7
felipeleite11 <lipi4shared@hotmail.com>
2022-05-11 01:20:04 -0300

    Add widget to mobile app.

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,20 +1,64 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useCallback, useEffect, useState } from 'react'
+import { StatusBar } from 'expo-status-bar'
+import { StyleSheet, View } from 'react-native'
+import * as SplashScreen from 'expo-splash-screen'
+import { useFonts, Inter_400Regular, Inter_500Medium } from '@expo-google-fonts/inter'
+
+import { theme } from './src/theme'
+
+import { Widget } from './src/components/Widget'
 
 export default function App() {
+  const [appIsReady, setAppIsReady] = useState(false)
+
+  const [fontsLoaded] = useFonts({
+    Inter_400Regular, 
+    Inter_500Medium
+  })
+
+  useEffect(() => {
+    async function prepare() {
+      try {
+        await SplashScreen.preventAutoHideAsync()
+      } catch (e) {
+        console.warn(e)
+      } finally {
+        setAppIsReady(true)
+      }
+    }
+
+    prepare()
+  }, [])
+
+  const onLayoutRootView = useCallback(async () => {
+    if (appIsReady) {
+      await SplashScreen.hideAsync()
+    }
+  }, [appIsReady])
+
+  if (!appIsReady || !fontsLoaded) {
+    return null
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View 
+      style={styles.container}
+      onLayout={onLayoutRootView}
+    >
+      <Widget />
+
+      <StatusBar 
+        style="light" 
+        backgroundColor="transparent"
+        translucent
+      />
     </View>
-  );
+  )
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+    backgroundColor: theme.colors.background
+  }
+})
